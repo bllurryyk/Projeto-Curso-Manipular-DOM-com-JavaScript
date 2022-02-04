bc3de2ee0967646909b6788e17376f0e6a883742
bllurryyk <khgds1@hotmail.com>
2022-02-04 16:51:27 -0300

    Funcionalidade Concluir Tarefas

--- lista-tarefas.js
+++ lista-tarefas.js
@@ -1,24 +1,42 @@
-const criarTarefa = (evento) => {
+(() => {
+    const criarTarefa = (evento) => {
 
-    evento.preventDefault();
-
-    const lista = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
-    const conteudo = `<p clas="content">${valor}</p>`;
-
-    tarefa.innerHTML = conteudo;
-
-    lista.appendChild(tarefa);
-    input.value = "";
-}
-
-const cliqueBotao = document.querySelector('[data-form-button]');
-
-cliqueBotao.addEventListener('click', criarTarefa);
-
-
-
+        evento.preventDefault();
+    
+        const lista = document.querySelector('[data-list]');
+        const input = document.querySelector('[data-form-input]');
+        const valor = input.value;
+    
+        const tarefa = document.createElement('li');
+        tarefa.classList.add('task');
+        const conteudo = `<p clas="content">${valor}</p>`;
+    
+        tarefa.innerHTML = conteudo;
+        lista.appendChild(tarefa);
+    
+        tarefa.appendChild(BotaoConcluir());
+        input.value = "";
+    };
+    
+    const cliqueBotao = document.querySelector('[data-form-button]');
+    
+    cliqueBotao.addEventListener('click', criarTarefa);
+    
+    // Component: Botão
+    const BotaoConcluir = () => {
+        const botaoConcluir = document.createElement('button');
+        botaoConcluir.classList.add('check-button');
+        botaoConcluir.innerText = 'Concluir';
+    
+        botaoConcluir.addEventListener('click', concluirTarefa);
+    
+        return botaoConcluir;
+    };
+    
+    const concluirTarefa = (evento) => {
+        const botaoConcluir = evento.target;
+        const tarefaFinalizada = botaoConcluir.parentElement;
+    
+        tarefaFinalizada.classList.toggle('done');
+    };
+})();
